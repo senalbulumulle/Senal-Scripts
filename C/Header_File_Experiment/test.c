a3d2eef772a75b2872c72b7010d8d0202e22ea4a
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-04-21 14:45:56 -0400

    Create test.c

---
+++ C/Header_File_Experiment/test.c
@@ -0,0 +1,17 @@
+/**************************************
+ Prototype 
+
+*/
+// C program to use the above created header file 
+#include <stdio.h> 
+#include "myhead.h" 
+int main() 
+{ 
+    synMath_add(4, 6); 
+
+    synMath_multiply(5, 5); 
+  
+    // Same for the multiply function in myhead.h 
+    printf("BYE!See you Soon"); 
+    return 0; 
+} 
