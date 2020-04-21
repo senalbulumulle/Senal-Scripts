e635be64f59b12ab3ad53329ee22f8b37e5596a0
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-04-21 14:45:03 -0400

    Create myhead.h

---
+++ C/Header_File_Experiment/myhead.h
@@ -0,0 +1,13 @@
+void synMath_Add(int a, int b) 
+{ 
+    printf("Added value=%d\n", a + b); 
+} 
+
+
+
+void synMath_Multiply(int a, int b) 
+{ 
+    printf("Multiplied value=%d\n", a * b); 
+} 
+
+
