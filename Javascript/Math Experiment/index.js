7f03be531873c9b0b859163e68aecf73ffad001e
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-04-28 16:12:14 -0400

    Javascript Math Experiment
     Description: This is for me to learn advanced equations in Programming

---
+++ Javascript/Math Experiment/index.js
@@ -0,0 +1,17 @@
+/************************************************************************
+ * Some Math Experiments
+ * By Senal Bulumulle
+ * Description: This is for me to learn advanced equations in Programming
+ * because my dad told me to learn advanced math equations. 
+ ***********************************************************************
+ */
+/****************************************************************** */
+ var x = 10; // X is the Variable of 10 
+ var Y = 30; // Y is the Variable of 30 
+ 
+// Use the PEMDAS Method 
+ var equation = 4 * (x + 20) - 20 * 9 / 3; // The Equation. Math Format 4x + 10 
+
+
+ document.write(equation); // Call the Equation using document.write()
+ console.log(equation); // Call the Equation Using console.log()
