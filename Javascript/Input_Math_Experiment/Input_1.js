31d77cdda0fcdefe3a601f2a00cfc29a1315faf5
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-04-29 08:21:47 -0400

    Input Math Experiment 1 Javascript

---
+++ Javascript/Input_Math_Experiment/Input_1.js
@@ -0,0 +1,29 @@
+/*
+# Some Math Experiments
+# Input Part 1
+# Description: This is for me to learn advanced equations in Programming
+# because my dad told me to learn advanced math equations
+# This also gives me a refresh from 9th grade 
+# Written by Senal Bulumulle
+#
+*/
+// HTML DOM in JS
+
+/**import "FOILOS.js"**/
+document.write("<style> h1{font-size:70px; font-family:verdana; background-color:black; color:white; padding:16px 32px; border-radius:10px;}{</style>");
+document.write("<style> body{font-size:30px; font-family:verdana; background-color:black; color:white; </style>");
+
+
+ 
+
+
+// x is the variable for user entering the first number 
+var x = prompt("Enter a Value for x");
+
+// y is the variable for user entering the second number
+var y = prompt("Now, Enter a Value for y ");
+// Only Times, Subtact, and Divide works but Addition doesn't work :-(
+
+x = 10;
+y = 11;
+document.write(x*y); // After the user answers the prompt, it should display the answer. 
---
+++ Javascript/Input_Math_Experiment/Input_1.sh
@@ -0,0 +1,36 @@
+# Some Math Experiments
+# Input Part 1
+# Description: This is for me to learn advanced equations in Programming
+# because my dad told me to learn advanced math equations
+# This also gives me a refresh from 9th grade 
+# Written by Senal Bulumulle
+# This File allows to run JS from CLI instead of UI directly. 
+
+## Make a Decorative Welcome Screen for CLI
+echo "#######################################################################"
+echo "                   Welcome to the Input System                         "
+echo "                    Written by Senal Bulumulle                         "
+echo "#######################################################################"                                     
+
+
+echo "#######################################################################"
+echo "                       Please Wait                                     "
+echo "                    Written by Senal Bulumulle                         "
+echo "#######################################################################" 
+
+
+
+
+## Launch the Python SimpleHTTPServer API 
+## 
+python -m SimpleHTTPServer 8000
+
+
+
+echo "#######################################################################"
+echo "                       Close the Terminal Window                       "
+echo "                      To Turn off the Localhost                        "
+echo "#######################################################################" 
+
+
+## Launch Google Chrome in Kiosk Mode After the Python SimpleHTTPServer API has successfully run. 
