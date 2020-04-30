6ba74f922d3acf354daba7df4940da37ca027689
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-04-30 08:31:57 -0400

    Javascript Math Experiment Practice 3

--- Javascript/Math_Experiment/Practice_3/README.md
+++ Javascript/Math_Experiment/Practice_3/README.md
@@ -1,2 +1,1 @@
 ## Practice 3
-### May use other JS libraries for complex math problems for this experiment
---
+++ Javascript/Math_Experiment/Practice_3/index.js
@@ -0,0 +1,33 @@
+/*****************************************************
+ *                  Practice 3 
+ *              Written by Senal Bulumulle
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ***************************************************
+ */
+document.write("<style> body{font-size:40px; background-color:black; color:white;} code{font-size:10px;}</style>");
+
+
+
+
+
+var z = !null; // Z is the variable as the answer to the equation of x and y 
+var x = 5*3; // 5x = 16;
+var y = 4*4; // y to the power of 2 
+var xy = 3*4; 
+var compile = x + y - xy;
+
+
+// Call out the variables using document.write()
+document.write(x);
+document.write("<ul> </ul>");
+document.write(y);
+document.write("<ul> </ul>");
+document.write(xy);
+document.write("<ul> </ul>");
+document.write(compile);
