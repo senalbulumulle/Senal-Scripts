30988fa035cfeba8618409c7a83d5f704f0908e5
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-07 15:56:47 -0400

    Javascript Math Experiment Practice 4

---
+++ Javascript/Math_Experiment/Practice_4/index.js
@@ -0,0 +1,26 @@
+/*****************************************************
+ *                  Practice 4 
+ *              Written by Senal Bulumulle
+ * 
+ * 
+ * 
+ *        Figure Out z = x2 + xy = y2
+ *      
+ * 
+ * 
+ * ***************************************************
+ */
+document.write("<style> body{font-size:40px; background-color:black; color:white;} code{font-size:10px;}</style>");
+
+ const z = 20; 
+ const x = 4*4;
+ const xy = 4*2;
+ const y = 2*2;
+
+ document.write(z);
+ document.write("<ul> </ul> ");
+ document.write(x);
+ document.write("<ul> </ul>");
+ document.write(xy);
+ document.write("<ul> </ul>");
+ document.write(y);
