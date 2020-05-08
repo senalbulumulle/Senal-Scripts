a97245ae248d32dafc6e5ecc93ad66c1294489a5
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-08 13:33:55 -0400

    Python Math Experiment Practice 1

---
+++ Python/Math_Experiment/Practice_1/index.py
@@ -0,0 +1,12 @@
+####################################################
+#              Math Experiments Python
+#                    Practice-1
+#             Written by Senal Bulumulle 
+####################################################
+
+## Declare a variable x Ask user to input value for x 
+x = input("Enter a Number for x:") 
+## Declare a variable Ask user to input value for y
+y = input("Enter a Number for y:")
+## Combine 
+print ("You Just Combined Into: " + x + y)  
