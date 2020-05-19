9c3e58f178a8f717d30b779ea5e6ca65c66e927c
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-19 10:11:07 -0400

    Create showCurrentTime.java

---
+++ Java/Practice_1/Mathematical Equations/src/showCurrentTime.java
@@ -0,0 +1,31 @@
+public class showCurrentTime {
+
+	public static void main(String[] args) {
+		// Obtain the total milliseconds since midnight. 
+		long totalMilliseconds = System.currentTimeMillis();
+		
+		// Obtain the total seconds since midnight, Jan 1, 1970
+		long totalSeconds = totalMilliseconds / 1000;
+		
+		// Compute the current second in the minute in the hour 
+		long currentSecond = totalSeconds % 60;
+		
+		// Obtain the total minutes 
+		long totalMinutes = totalSeconds / 60;
+		
+		// Compute the current minute in the hour.
+		long currentMinute = totalMinutes % 60;
+		
+		// Obtain the total hours 
+		long totalHours = totalMinutes / 60;
+		
+		// Compute the current hour
+		long currentHour = totalHours % 24;
+		
+		// Display Results 
+		System.out.println("Current time is: " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
+		
+
+	}
+
+}
