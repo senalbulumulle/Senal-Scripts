410a8ebac9ef36592e563a34b0c01b4f3db04df5
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-19 08:32:35 -0400

    Forgot to put the : from Line 22 to Line 25 (OrderTwoCities.java)

--- Java/Practice_1/Senal Practice/src/OrderTwoCities.java
+++ Java/Practice_1/Senal Practice/src/OrderTwoCities.java
@@ -19,10 +19,10 @@ public class OrderTwoCities {
 		String city2 = input.nextLine();
 		
 		if (city1.compareTo(city2) < 0 )
-			System.out.println("The cities in alphabetical order are " + city1 + " " + city2);
+			System.out.println("The cities in alphabetical order are:  " + city1 + " " + city2);
 		
 		else
-			System.out.println("The cities in alphabetical order are " + city2 + " " + city1);
+			System.out.println("The cities in alphabetical order are: " + city2 + " " + city1);
 		
 	}
 
