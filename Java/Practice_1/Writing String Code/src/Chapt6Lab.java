cf9cef153b5463d6c18ec6e22eb625519c36b732
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-20 16:57:33 -0400

    Create Chapt6Lab.java
    This file is located in "Senal-Scripts/Java/Practice-1/Writing String Code/src/Chapt5lab.java".

---
+++ Java/Practice_1/Writing String Code/src/Chapt6Lab.java
@@ -0,0 +1,19 @@
+public class Chapt6Lab {
+
+	public static void main(String[] args) {
+		// "sayHello" = String Variable
+		sayHello("Grumpy");
+		sayHello("Happy");
+		sayHello("Sleepy");
+		sayHello("dwarf 4");
+		sayHello("dwarf 5");
+		sayHello("dwarf 6");
+		sayHello("dwarf 7");
+
+	}
+	public static void sayHello (String name){
+		// Displays all of the strings based on the string variable "sayHello".
+		System.out.println("Hello. " + name + "!");
+	}
+
+}
