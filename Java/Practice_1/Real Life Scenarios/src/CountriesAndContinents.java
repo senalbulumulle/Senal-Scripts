93e929e6b163f61c0547ad0c66ce8b75b4923717
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-05-21 10:38:00 -0400

    Create CountriesAndContinents.java
    This file is located in "Senal-Scripts/Java/Practice_1/Real Life Scenarios/src/CountriesAndContinents.java".

---
+++ Java/Practice_1/Real Life Scenarios/src/CountriesAndContinents.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class CountriesAndContinents {
+
+	public static void main(String[] args) {
+		// Create Scanner object
+		Scanner input = new Scanner (System.in);
+
+		// Prompt User to enter a country
+		System.out.print("Enter country name: ");
+		String country = input.nextLine();
+
+		// Sort countries into continents. 
+
+		if (country.equals("United States") || 
+					country.equals("Canada")||
+					country.equals("Mexico"))
+					System.out.print(country + " is in North America");
+		else if (country.equals("Brazil") || 
+					country.equals("Colombia") ||
+					country.equals("Peru")  ||
+					country.equals("Chile") ||
+					country.equals("Argentina")) 
+				System.out.print(country + " is in South America");
+		else if (country.equals("Europe") ||
+					country.equals("Germany") ||
+					country.equals("England") ||
+					country.equals("Italy") ||
+					country.equals("France") ||
+					country.equals("Greece") ||
+					country.equals("Norway") ||
+					country.equals("Spain") ||
+					country.equals("Belgium") ||
+					country.equals("Switzerland"))
+				System.out.print(country + " is in Europe");
+		else if (country.equals("Egypt") ||
+					country.equals("South Africa") ||
+					country.equals("Nigeria") ||
+					country.equals("Ethopia") ||
+					country.equals("Libya") ||
+					country.equals("Madagascar") ||
+					country.equals("Morocco") ||
+					country.equals("Angola") ||
+					country.equals("Kenya")) 
+				System.out.print(country + " is in Africa");
+		else if (country.equals("China") ||
+					country.equals("India") ||
+					country.equals("Japan") ||
+					country.equals("Sri Lanka") ||
+					country.equals("South Korea") ||
+					country.equals("North Korea") ||
+					country.equals("Thailand") ||
+					country.equals("Vietnam") ||
+					country.equals("Cambodia"))
+				System.out.print(country + " is in Asia");
+		else 
+				System.out.print("I don't know where " + country + " is. " );
+
+		input.close();
+
+
+	}
+
+}
