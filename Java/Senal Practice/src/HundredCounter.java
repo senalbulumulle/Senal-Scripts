b90ca1885ff47732e5130a3acf5d91f996e0312b
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-06-29 13:13:40 -0400

    Create HundredCounter.java
    This file is located in "Senal-Scripts/Java/Senal Practice/src/HundredCounter.java".

---
+++ Java/Senal Practice/src/HundredCounter.java
@@ -0,0 +1,16 @@
+public class HundredCounter {
+
+	public static void main(String[] args) {
+		
+		// Make an int variable called Counter
+		int counter = 1;
+		
+		// Make a while loop on counter, so it will count numbers until 100. 
+		while (counter <= 100) {
+			System.out.println(counter);
+			counter++;
+			
+		}
+	}
+
+}
