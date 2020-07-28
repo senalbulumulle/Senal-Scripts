501338644a5beed33ae4d24ad495ee6b858e000c
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-07-28 09:44:40 -0400

    Create Student.java
    This file is located in "Senal-Scripts/Java/Senal Practice/src/Student.java".

---
+++ Java/Senal Practice/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+	
+	int studentId;
+	String name;
+	
+	Student(int studentId, String name) {
+		this.studentId = studentId;
+		this.name = name;
+	}
+	
+	public String toString() {
+		return studentId + " " + name;
+	}
+
+}
