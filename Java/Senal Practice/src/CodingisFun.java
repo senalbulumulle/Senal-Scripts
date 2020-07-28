d64aca28eb79c3c8f74b49e460d2bee62dfbfab3
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-07-28 09:43:06 -0400

    Create CodingisFun.java
    This file is located in "Senal-Scripts/Java/Senal Practice/src/CodingisFun.java".

---
+++ Java/Senal Practice/src/CodingisFun.java
@@ -0,0 +1,15 @@
+public class CodingisFun {
+
+	public static void main(String[] args) {
+		
+		// Call the method student
+		Student s1 = new Student(1, "Bruce");
+		Student s2 = new Student(2, "Jackie");
+		
+		// Print out the student names
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+
+}
