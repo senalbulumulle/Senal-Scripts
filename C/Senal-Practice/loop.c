f9f423184a52eb1505bcf53a662a062b159067da
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-09-01 17:02:47 -0400

    Create loop.c
    This file is located in "Senal-Scripts/C/Senal-Practice/loop.c".

---
+++ C/Senal-Practice/loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	// Make an int variable called i 
+	int i;
+
+	// Make a for-loop 
+	for (i = 1; i < 11; ++i)
+	{
+		printf("%d ", i);
+	}
+
+	// End the program
+	return 0;
+}
