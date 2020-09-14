42504dc9fff0178a2f71903e820a5d09415650ca
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-09-14 16:58:39 -0400

    Create while-loop-syntax.c
    This file is located in "Senal-Scripts/C/Senal-Practice/while-loop-syntax.c".

---
+++ C/Senal-Practice/while-loop-syntax.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\n\n\t\tStudytonight - Best Place to learn\n\n\n");
+
+
+	// Always Declare the variables before using them
+
+
+	// Make an int variable called "i"
+	int i = 0;
+
+
+	// Print the numbers using the loop
+	printf("\nPrinting numbers using while loop from 0 to 9\n\n");
+
+
+
+	/* 
+		While i is less than 10
+
+	*/
+
+	/* While loop */
+
+	
+	// Make a while loop
+	while (i < 10)
+	{
+		printf("%d\n", i);
+
+		/*  
+		Update i so the condition can be met eventually to terminate the loop
+
+		*/
+
+		i++;
+	}
+
+	// Print the screen
+	printf("\n\n\t\tCoding is fun\n\n\n");
+
+	// End the program
+	return 0;
+	
+}
