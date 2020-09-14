7965b8287a6e4018b57dc44fc629e6df9fa3b4d3
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-09-14 17:06:01 -0400

    Create do-while-loop-syntax.c
    This file is located in "Senal-Scripts/C/Senal-Practice/do-while-loop-syntax.c".

---
+++ C/Senal-Practice/do-while-loop-syntax.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+
+	// Print the title 
+	printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+	/* Always declare the variables before using them */
+
+	/* Int variable */
+
+	// Make an int variable called i 
+	int i = 10;
+
+	// do contains the actual code and the updation
+
+	do 
+	{
+		printf("i = %d\n", i);
+
+		// the updation
+		i = i-1;
+	}
+
+	/* While Loop */
+
+	// Make a while loop
+	while (i > 0);
+
+
+
+	// Print statements
+	printf("\nThe value of i after exiting the loop is %d\n\n", i);
+	printf("\n\n\t\tCoding is fun !\n\n\n");
+
+	// End the program
+	return 0;
+
+	
+}
