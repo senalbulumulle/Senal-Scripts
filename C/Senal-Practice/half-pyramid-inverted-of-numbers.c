e8b4a78243408871444d3e76c4fb202a2d7465a7
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2020-09-30 09:53:52 -0400

    Create half-pyramid-inverted-of-numbers.c
    This file is located in "Senal-Scripts/C/Senal-Practice/half-pyramid-inverted-of-numbers.c".

---
+++ C/Senal-Practice/half-pyramid-inverted-of-numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+	// Make int variables called i, j, and rows
+	int i, j, rows;
+
+	
+	// Prompt the user to enter the number of rows
+	printf("Enter the number of rows: ");
+
+	// Make a scanner object
+	scanf("%d", &rows);
+
+	// Make a for loop
+	for (i = rows; i >= 1; --i) {
+
+		for (j = 1; j <= i; ++j) {
+
+			printf("%d ", j);
+		}
+
+		printf("\n");
+	} 
+
+	// End the program 
+	return 0;
+}
