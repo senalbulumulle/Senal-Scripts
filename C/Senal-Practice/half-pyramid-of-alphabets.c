3eb7408d0370a44d8e4628e241c30b1f823c3b86
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2020-09-30 09:20:16 -0400

    Create half-pyramid-of-alphabets.c
    This file is located in "Senal-Scripts/C/Senal-Practice/half-pyramid-of-alphabets.c".

---
+++ C/Senal-Practice/half-pyramid-of-alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+	// Make an int variable called i, and j
+	int i, j; 
+
+	// Char Variables 
+	char input, alphabet = 'A';
+
+	// Prompt the user to enter an uppercase character
+	// that they want to print in the last row
+	printf("Enter an uppercase character you want to print in the last row: ");
+
+	// Make a scanner object
+	scanf("%c", &input);
+
+	// Make a for loop
+	for (i = 1; i <= (input - 'A' + 1); ++i) {
+
+		for (j = 1; j <= i; ++j) {
+
+			printf("%c", alphabet);
+		}
+
+		
+		++alphabet;
+
+		
+		printf("\n");
+	}
+
+	// End the program 
+	return 0;
+}
