f831ea6275aeb3709c7ac2b4b40a305043bb25bc
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2020-10-07 17:47:44 -0400

    Create gcd.c
    This file is located in "Senal-Scripts/C/Senal-Practice/gcd.c".

---
+++ C/Mathematical-Equations/gcd.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Make int variable 
+int find_gcd(int, int);
+
+int main()
+{
+
+	printf("\n\n\t\tSenal Programming Language - Best place to learn");
+
+	// Make int variables
+	int a, b, gcd; 
+
+	// Prompt the user to enter the two numbers to find GCD of something
+	printf("\n\nEnter two numbers to find GCD of\n");
+
+	// Make a scanner object 
+	scanf("%d%d", &a, &b);
+
+	gcd = find_gcd(a, b);
+
+	printf("\n\nGCD of %d and %d is: %d\n\n", a, b, gcd);
+
+	printf("\n\n\t\t\tSenal Bulumulle likes making things\n\n\n");
+
+	return 0;
+}
+
+// Defining the function
+int find_gcd(int x, int y)
+{
+	if (x > y)
+		find_gcd(x - y, y);
+
+	else if (y > x)
+		find_gcd(x, y - x);
+
+	else
+	return x;
+}
