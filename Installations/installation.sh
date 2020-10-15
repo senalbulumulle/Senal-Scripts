f32a97f08c2632ae461d208a34febd44a6fb416e
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2020-10-15 09:35:45 -0400

    Create Installations Folder
    This file is located in "Senal-Scripts/Installations".

---
+++ Installations/installation.sh
@@ -0,0 +1,52 @@
+##  Installation Script for Ubuntu Server 
+##  Written by Senal Bulumulle
+##  This Script allows the system to install some of the applications and dependencies that are used for coding.
+
+## Using APT Package Manager, Install Some Dependencies and Applications that are used for the Ubuntu Server
+
+## Make Sure System needs updates
+sudo apt upgrade
+
+## Install 'git' dependency
+sudo apt install git
+
+## Install 'firefox' dependency
+sudo apt install firefox
+
+## Install 'apache2' and 'php'
+sudo apt install apache2 php 
+
+## Install 'node.js' dependency
+sudo apt install nodejs
+
+## Install 'gnome-desktop-environment' dependency
+sudo apt install gnome-session
+
+## Install 'lightdm' dependency for login
+sudo apt install lightdm
+
+## Uncomment the 'ubuntu-desktop' dependency if wanted UI (Yaru Theme for Gnome Desktop Environment)
+## sudo apt install ubuntu-desktop
+
+## Install 'gdebi' GUI Package Manager Application for install Debian Packages
+sudo apt install gdebi
+
+## Install 'dpkg' to install packages without graphical interface
+sudo apt install dpkg
+
+## Install 'wget' to install packages without graphical interface
+sudo apt install wget
+
+## Install Google Chrome using wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+## Using wget, install github desktop for ubuntu 
+wget https://github.com/shiftkey/desktop/releases/download/release-2.5.3-linux1/GitHubDesktop-linux-2.5.3-linux1.deb
+
+## Prompt user that Script is ended 
+echo "######################################################"
+echo "                Script has ended                      "
+echo "	       	     Please Restart Your System             " 
+echo "######################################################"
+
+
---
+++ Installations/installation_clib.sh
@@ -0,0 +1,13 @@
+## Installation for the clib package manager 
+
+## Clone the repository from git
+git clone https://github.com/clibs/clib.git /tmp/clib
+
+## Change directory to /tmp/clib/
+cd /tmp/clib
+
+## Unlock /usr/bin
+sudo chmod 777 /usr/bin
+
+## Install the clib package manager using MAKE
+make install
