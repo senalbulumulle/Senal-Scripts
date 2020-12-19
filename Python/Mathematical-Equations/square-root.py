5242e332a5c5be03d77a62184fe1ad7e0568f736
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-12-19 10:05:08 -0500

    Create square-root.py
    This file is located in "Senal-Scripts/Python/Mathematical-Equations/square-root.py

---
+++ Python/Mathematical-Equations/square-root.py
@@ -0,0 +1,13 @@
+## square-root.py
+
+## Make a variable called num
+num = 8
+
+## Prompt the user to enter a number 
+#num = float(input('Enter a number:'))
+
+## Calculate the Square root
+num_sqrt = num ** 0.5
+
+## Display the square root to the user 
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
