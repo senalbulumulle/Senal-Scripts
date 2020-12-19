e3053aee06cbbf1ed66cd95924c84b04cbf3bcb2
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-12-19 09:45:40 -0500

    Create add-two-numbers.py
    This file is located in "Senal-Scripts/Python/Mathematical-Equations/add-two-numbers.py".

---
+++ Python/Mathematical-Equations/add-two-numbers.py
@@ -0,0 +1,17 @@
+## add-two-numbers.py
+
+## Make a variable called num1
+num1 = 1.5
+
+## Make a variable called num2
+num2 = 6.3
+
+
+## Use sum as the the answer and combine with num1 
+## and num2
+sum = num1 + num2
+
+
+## Display the answer (sum) to the user 
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
