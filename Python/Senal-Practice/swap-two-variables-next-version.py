ae08f1855be7dbc5539fccc33a7c7f2c60721694
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-12-21 09:12:46 -0500

    Create swap-two-variables-next-version.py
    This file is located in "Senal-Scripts/Python/Senal-Practice/swap-two-variables-next-version.py".

---
+++ Python/Senal-Practice/swap-two-variables-next-version.py
@@ -0,0 +1,16 @@
+# Python program to swap two variables 
+# Second version
+x = 7
+y = 2
+
+# To take inputs from the user 
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values 
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
