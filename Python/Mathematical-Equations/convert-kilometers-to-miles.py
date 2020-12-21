6120122164aa27993f824f81e07384d2d6a9da58
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-12-21 09:19:28 -0500

    Create convert-kilometers-to-miles.py
    This file is located in "Senal-Scripts/Python/Mathematical-Equations/convert-kilometers-to-miles.py".

---
+++ Python/Mathematical-Equations/convert-kilometers-to-miles.py
@@ -0,0 +1,6 @@
+# convert-kilometers-to-miles.py
+
+kilometers = float(input("Enter value in kilometers:  "))
+conv_fac = 0.621371
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
