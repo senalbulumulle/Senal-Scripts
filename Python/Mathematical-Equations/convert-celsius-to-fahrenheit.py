af8914aa6cce52f79a826ad82ca1d6186d4fb3fe
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-12-21 09:26:58 -0500

    Create convert-celsius-to-fahrenheit.py
    This file is located in "Senal-Scripts/Python/Mathematical-Equations/convert-celsius-to-fahrenheit.py".

---
+++ Python/Mathematical-Equations/convert-celsius-to-fahrenheit.py
@@ -0,0 +1,13 @@
+# convert-celsius-to-fahrenheit.py
+
+# Description: This program allows the convertion from celsius to fahrenheit
+
+# Make a variable called celsius that contains a number that is used 
+# for convertion
+celsius = 37.5
+
+# Make a variable called fahrenheit to calculate the provided number in celsius
+fahrenheit = (celsius * 1.8) + 32
+
+# Display the output
+print('%0.1f degree celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
