cf182a9eef9cea1c9b89a5dc7e8c1d4cd71110be
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-24 18:04:49 -0400

    Updated

---
+++ D/hello_world.d
@@ -0,0 +1,16 @@
+/**
+ *  hello_world.d
+ *
+ */
+
+
+// Import the libraries here
+import std.stdio;
+
+
+// Create a void function called main().
+void main() 
+{
+    // Print out hello world
+    writeln("Hello, World!");
+}
---
+++ D/run.sh
@@ -0,0 +1,3 @@
+## run.sh
+
+dmd hello_world.d                               ## Compile
