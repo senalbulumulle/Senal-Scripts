e0d81619c187432a5636c0d6561a65cc03b70b47
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-24 23:44:08 -0400

    Updated

---
+++ Lua/functions.lua
@@ -0,0 +1,15 @@
+-- --------------------------
+-- functions.lua
+-- ---------------------------
+
+
+
+-- Create a function called hello().
+function hello()
+    print("hello function!");
+end
+
+
+
+-- This is where we are able to call the function.
+hello();
