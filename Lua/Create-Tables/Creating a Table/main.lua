c58a9524289603b81328162f336d32ba1116efc7
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-25 13:28:14 -0400

    Updated

---
+++ Lua/Create-Tables/Creating a Table/main.lua
@@ -0,0 +1,18 @@
+-- Creating a table -- 
+
+-- Create a function called initTable() --
+function initTable()
+    local theTable = {}
+end
+
+-- Create a function called createTable() --
+function createTable()
+    name   = "Hello"
+    number = "123"
+    more   = {"Senal", "is", "Senal"}
+end
+
+
+initTable()
+createTable()
+
