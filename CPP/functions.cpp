04dccfc05095ce15b2b98112e22353b6de821b2b
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-29 10:43:22 -0400

    Updated

---
+++ CPP/functions.cpp
@@ -0,0 +1,20 @@
+/**
+Functions.cpp
+**/
+
+
+// include the libraries here
+#include <iostream>
+#include <stdlib.h>
+
+
+void hello_function()
+{
+    std::cout << "hello function" << std::endl;
+}
+
+
+int main()
+{
+    hello_function();
+}
---
+++ CPP/run.sh
@@ -0,0 +1,4 @@
+## run.sh
+
+g++ hello_world.cpp -o hello_world                              ## Compile
+g++ functions.cpp -o functions                                  ## Compile
