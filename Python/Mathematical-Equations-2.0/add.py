0433f15a1dba172901d54e57852c64842e0ef6c3
senalbulumulle <senal.bulumulle@gmail.com>
2024-10-12 16:59:27 +0000

    Document

--- Python/Mathematical-Equations-2.0/add.py
+++ Python/Mathematical-Equations-2.0/add.py
@@ -3,7 +3,11 @@ import os
 import sys
 import json
 
-
+'''
+This is where we create a function called equation
+which contains numbers that will be used to calculate.
+Followed by the calculate_equation function
+'''
 def equation():
     first_number  = 20
     second_number = 20
