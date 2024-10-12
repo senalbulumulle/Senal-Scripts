be3bbf99b60242dc6adfe49d72aa86954161c98f
senalbulumulle <senal.bulumulle@gmail.com>
2024-10-12 16:49:58 +0000

    Added [add.py]

---
+++ Python/Mathematical-Equations-2.0/add.py
@@ -0,0 +1,17 @@
+import math
+import os
+import sys
+import json
+
+
+def equation():
+    first_number  = 20
+    second_number = 20
+equation()
+
+
+
+def calculate_equation():
+    equation()
+    first_number + second_number
+calculate_equation
