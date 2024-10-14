64913bba1e87361cca5f2221d4fddc300712075d
senalbulumulle <senal.bulumulle@gmail.com>
2024-10-14 16:17:56 +0000

    Removed the functions lol

--- Python/Mathematical-Equations-2.0/add.py
+++ Python/Mathematical-Equations-2.0/add.py
@@ -4,18 +4,14 @@ import sys
 import json
 
 '''
-This is where we create a function called equation
-which contains numbers that will be used to calculate.
-Followed by the calculate_equation function
+Create two variables
+
+
+1. first_number
+2. second_number
 '''
-def equation():
-    first_number  = 20
-    second_number = 20
-equation()
 
+first_number  = 20
+second_number = 20
 
-
-def calculate_equation():
-    equation()
-    first_number + second_number
-calculate_equation
+first_number + second_number
