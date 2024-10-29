7d961f4e74b2104d76d92c7cc5865c9e9df2860a
senalbulumulle <senal.bulumulle@gmail.com>
2024-10-29 12:16:17 -0400

    Created an template

---
+++ Learn-React/organize-components/src/Template.js
@@ -0,0 +1,17 @@
+// template.js
+
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function Template() {
+    return (
+      <div>
+            
+      </div>
+    );
+  }
+  
+export default Template;
+  
