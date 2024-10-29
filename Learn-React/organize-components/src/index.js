0bc065d406ff2f00fc957098eaa77e6d0c85d73b
senalbulumulle <senal.bulumulle@gmail.com>
2024-10-29 12:56:03 -0400

    Include

--- Learn-React/organize-components/src/index.js
+++ Learn-React/organize-components/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+
+import Buttons from './Button.js';
+import Spotlights from './Spotlights.js';
+import Other from './Other.js';
+
+
+
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
