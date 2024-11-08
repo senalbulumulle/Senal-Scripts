c5621c51a18554535cf430b3ac50773696754a8b
senalbulumulle <senal.bulumulle@gmail.com>
2024-11-08 16:02:02 -0500

    oh?

--- Rust/arrays/src/main.rs
+++ Rust/arrays/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+ 	// Here, this is where we are able to create an variable
+	// which stores that array of numbers 
+
+	let numberarray = [10, 20, 30, 40, 50]; 
+
+
+	// Here, this is where we are able to pick the array
+	// using the picknumberarray variable, which is also 
+	// used to initialize .len() feature, also with other
+	// numberarray.<> features.
+	
+	let picknumberarray = numberarray.len();
+
 }
---
+++ Rust/arrays/target/debug/arrays.d
@@ -0,0 +1,1 @@
+/home/senal/Code/senal-scripts/Rust/arrays/target/debug/arrays: /home/senal/Code/senal-scripts/Rust/arrays/src/main.rs
