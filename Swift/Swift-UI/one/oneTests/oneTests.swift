f6c1fda86e471872856a6cee794815f559861dcd
senalbulumulle <senal.bulumulle@gmail.com>
2024-11-18 15:42:53 -0500

    Created new skill issue: one [on Swift UI]

---
+++ Swift/Swift-UI/one/oneTests/oneTests.swift
@@ -0,0 +1,17 @@
+//
+//  oneTests.swift
+//  oneTests
+//
+//  Created by Senal on 11/18/24.
+//
+
+import Testing
+@testable import one
+
+struct oneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
