daf9c4036580ed60b92d8b4c867f0fea787aeb62
senalbulumulle <senal.bulumulle@gmail.com>
2024-11-18 16:08:26 -0500

    [two]

---
+++ Swift/Swift-UI/two/one/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  one
+//
+//  Created by Senal on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        // Definition:
+        
+        // Think of VStack as the vertical view
+        
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/Swift-UI/two/one/oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneApp.swift
+//  one
+//
+//  Created by Senal on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
