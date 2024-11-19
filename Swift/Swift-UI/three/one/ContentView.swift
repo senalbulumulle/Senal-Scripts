74ce39de644fb21769422f8a0b56d9b3073e924a
senalbulumulle <senal.bulumulle@gmail.com>
2024-11-19 10:36:11 -0500

    [three]

---
+++ Swift/Swift-UI/three/one/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  one
+//
+//  Created by Senal on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationStack 
+        {
+            ZStack 
+            {
+                LinearGradient(gradient: Gradient(colors: [.purple, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+
+                    // Full thing
+                    .ignoresSafeArea() 
+
+                VStack 
+                {
+                    Text("Hello, World!")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .padding()
+
+                    // Button("Tap Me") {
+                    //     // Perform action here
+                    // }
+                }
+            }
+            
+            .navigationTitle("Something Something") 
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
