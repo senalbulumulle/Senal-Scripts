a7f7fafa1984acdf26270ea3c8337e09a6503dbc
senalbulumulle <senal.bulumulle@gmail.com>
2024-11-18 21:32:15 -0500

    that might work

--- Swift/Swift-UI/two/one/ContentView.swift
+++ Swift/Swift-UI/two/one/ContentView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.purple, .green]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+        NavigationStack 
+        {
+            ZStack 
+            {
+                LinearGradient(gradient: Gradient(colors: [.purple, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+
+                    .ignoresSafeArea() 
+
+                VStack 
+                {
+                    Text("Hello, World!")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .padding()
+
+                    // Button("Tap Me") {
+                    //     // Perform action here
+                    // }
+                }
+            }
+            
+            .navigationTitle("Something Something") 
         }
     }
 }
